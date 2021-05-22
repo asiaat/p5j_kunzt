c4611021ad96f62ebe4bb8a3d827dc346fcab64b
Kalle Olumets <olumetskalle@gmail.com>
2021-05-22 11:51:36 +0300

    snowflakes

--- src/bubbles.js
+++ src/bubbles.js
@@ -3,11 +3,12 @@
 let bubble1;
 let bubble2;
 
+
 function setup() {
-  import Bubble from './src/bubble';  
+   
   createCanvas(600, 400);
   bubble1 = new Bubble(300, 200, 40);
-  //bubble2 = new Bubble(400, 200, 20);
+  bubble2 = new Bubble(400, 200, 20);
   
 }
 
@@ -15,8 +16,30 @@ function draw() {
   background(0);
   bubble1.move();
   bubble1.show();
-  //bubble2.move()
-  //bubble2.show();
+  bubble2.move()
+  bubble2.show();
   
+}
+
+
+class Bubble {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+
+  move() {
+    this.x = this.x + random(-5, 5);
+    this.y = this.y + random(-5, 5);
+   
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    ellipse(this.x, this.y, this.r * 2);
+  }
 }
 
